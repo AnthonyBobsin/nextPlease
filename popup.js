5f6913f861d30aac3fb907cb202d37cb4ff9e351
AnthonyBobsin <bobsinj@gmail.com>
2016-06-19 14:07:45 -0400

    Initialize repo with "Hello World" extension

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var alertButton = document.getElementById('alertButton')
+  alertButton.addEventListener('click', function() {
+    alert("Hello World!!!")
+  }, false)
+}, false)
